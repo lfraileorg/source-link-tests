fec1b08a1a598f7c8f5c17b85c8d3c4b4c16c200
Luis Fraile <lfraile@lfraile.net>
2023-05-05 09:28:28 +0200

    First commit

---
+++ Hello.World.Lfraile/Hello.World.Lfraile/Salute.cs
@@ -0,0 +1,11 @@
+﻿namespace Hello.World.Lfraile
+{
+    public static class Salute
+    {
+        public static string You(string you)
+        {
+            return $"Hello {you}";
+        }
+
+    }
+}
