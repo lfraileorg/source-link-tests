75196291ce416cc6074d8edb7d7995e12a93d481
Luis Fraile <lfraile@lfraile.net>
2023-05-08 08:57:57 +0200

    v388

--- Hello.World.Lfraile/Hello.World.Lfraile/Salute.cs
+++ Hello.World.Lfraile/Hello.World.Lfraile/Salute.cs
@@ -4,7 +4,7 @@
     {
         public string You(string you)
         {
-            return $"Hello {you} this is new!!! 387";
+            return $"Hello {you} this is new!!! 388";
         }
 
     }
